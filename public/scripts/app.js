9ac8606b939ca6c0bf4302bdfe727cb6140d2f7c
Terrence <terrence.liu@alumni.ubc.ca>
2023-09-17 23:55:43 -0700

    Fix drop-down menu bug with padding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,14 +6,14 @@ $(document).ready(() => {
   $("#dropdown-menu-content").hide();
 
   // reveal drop-down menu on click
-  $("#dropdown-menu").click((event) => {
+  $("#dropdown-menu-icon").click((event) => {
     $("#dropdown-menu-content").toggle();
   });
 
   $(document).click((event) => {
 
     // Click occurred outside the dropdown menu and button
-    if (!$(event.target).closest("#dropdown-menu-content").length && !$(event.target).is("#dropdown-menu")) {
+    if (!$(event.target).closest("#dropdown-menu-content").length && !$(event.target).is("#dropdown-menu-icon")) {
       $("#dropdown-menu-content").hide();
     }
 
