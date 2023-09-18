0347cba2f91539df1a101af8619b92521cc3f7e2
Terrence <terrence.liu@alumni.ubc.ca>
2023-09-17 23:01:34 -0700

    Implement drop-down menu logic

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,21 @@
 // Client facing scripts here
+
+$(document).ready(() => {
+  
+  // hide drop-down menu on load
+  $("#dropdown-menu-content").hide();
+
+  // reveal drop-down menu on click
+  $("#dropdown-menu").click((event) => {
+    $("#dropdown-menu-content").toggle();
+  });
+
+  $(document).click((event) => {
+
+    // Click occurred outside the dropdown menu and button
+    if (!$(event.target).closest("#dropdown-menu-content").length && !$(event.target).is("#dropdown-menu")) {
+      $("#dropdown-menu-content").hide();
+    }
+
+  });
+});
