a0a82844aa85a91926eab30d7c1a6b8e02a07820
Feda Muhammadian <fedamuhammadian@Fedas-MacBook-Air.local>
2023-09-18 17:33:35 -0700

    routes updated

--- db/connection.js
+++ db/connection.js
@@ -2,15 +2,29 @@
 const { Pool } = require('pg');
 
 const dbParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'admin',
+  database: 'midterm',
 };
 
 const db = new Pool(dbParams);
 
-db.connect();
+db.connect().then(() => {
+  console.log("Connected to Database!!");
+});
+
+/* 
+db.query ('select * from users', (err,res) =>{
+  if (!err) {
+    console.log(res.rows);
+  }
+  else {
+    console.log (err.message);
+  }
+  db.end
+})
+*/
 
 module.exports = db;
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@
 
 const express = require('express');
 const router  = express.Router();
+const db = require ('db')
 
 router.get('/', (req, res) => {
   res.render('users');
@@ -16,12 +17,57 @@ router.get('/signin', (req, res) => {
   res.render('signin');
 });
 
-router.post('/signin', (req, res) => {
-  
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Query the database to find the user with the provided email and password
+    const user = await db.oneOrNone(
+      'SELECT * FROM Users WHERE email = $1 AND password = $2',
+      [email, password]
+    );
+
+    if (user) {
+      // User with matching email and password found
+      res.status(200).json({ message: 'Authentication successful', user });
+    } else {
+      // No user found with the provided username and email
+      res.status(401).json({ error: 'Authentication failed' });
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
-router.get('/createaccount', (req, res) => {
-  res.render('createaccount');
+
+router.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  res.render('users', { id });
 });
+
+
+router.get('/createaccount', (req, res) => {
+  res.render('/createaccount');
+});
+
+router.post('/createaccount', async (req, res) => {
+  try {
+    const { first_name, last_name, email, password } = req.body;
+
+    const newUser = await db.one(
+      'INSERT INTO Users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING id',
+      [first_name, last_name, email, password]
+    );
+
+    // Redirect to the user page using the newly created user's ID
+    res.redirect(`/user/${newUser.id}`);
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).send('Error creating user');
+  }
+});
+
 
 module.exports = router;
