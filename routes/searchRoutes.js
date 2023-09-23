5cab7737b20101bff8ced842e483c52ef87e0831
Abdul <lhlrahman@gmail.com>
2023-09-23 02:54:00 -0400

    route for adv and reg search

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -2,32 +2,49 @@ const express = require('express');
 const router  = express.Router();
 const search = require('../db/queries/searches');
 const post = require('../db/queries/post');
-const searchInput = require('../public/scripts/search');
 
-
-router.get("/results", (req, res) => {
-  console.log(req.query.search);
-  const templateVars = {
-    search: search.regSearch(req.query.search)
-  };
-  res.render("results", templateVars);
+router.get("/results/:query", async (req, res) => {
+  if (req.params.query === "undefined") {
+    return res.status(400).send("Invalid search query");
+  }
+  try {
+    console.log("Fetching results for:", req.params.query); // Debug line
+    let searchResults = await search.regSearch(req.params.query);
+    console.log("Search results:", searchResults); // Debug line
+    if (searchResults && searchResults.length > 0) {
+      res.redirect(`/listings/${searchResults[0].id}`);
+    } else {
+      res.send('No results found for your query');
+    }
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
 });
 
 
-router.post("/search", async (req, res) => {
+
+
+router.post("/search", (req, res) => {
  try {
-  const searchResult = await search.regSearch(req.body.searchTerm);
-  const templateVars = {
-    search: searchResult[0]
-   };
-  console.log(templateVars.search, "templateVars.search");
-  res.render("results", templateVars);
+  console.log("Searching for:", req.body); // Debug liner
+   res.redirect(`results/${req.body.searchTerm}`);
 } catch (err) {
-  // handle error
   res.status(500).send('Server Error');
 }
-
 });
 
+
+router.get("/advsearch", (req, res) => {
+  res.render('advsearch');
+});
+
+router.post("/advsearch", (req, res) => {
+  console.log("Searching for:", req.body); // Debug liner
+  search.advSearch(req.body)
+  .then(results => {
+    console.log("Search results:", results); // Debug line
+    res.redirect(`/listings/${results[0].id}`);
+});
+});
 
 module.exports = router;
